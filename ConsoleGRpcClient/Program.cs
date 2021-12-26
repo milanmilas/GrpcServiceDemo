da514ee40790924f5f81ccedf5f32c1956c49e3f
Milan Milas <milanmilas@gmail.com>
2021-12-26 12:14:06 +0000

    Add project files.

---
+++ ConsoleGRpcClient/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Grpc.Net.Client;
+using GrpcServiceDemo;
+
+Console.WriteLine("Hello, World!");
+
+var channel = GrpcChannel.ForAddress("https://localhost:7269");
+var client = new Greeter.GreeterClient(channel);
+
+var reply = await client.SayHelloAsync(new HelloRequest
+{
+    Name = ".NET Conf"
+});
+
+Console.WriteLine($"From server '{reply.Message}'");
